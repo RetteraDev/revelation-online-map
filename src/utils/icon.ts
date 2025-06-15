e2cc241c19abc221b7ead43dfe7486f52f753910
Rettera <trof1m00@mail.ru>
2025-06-15 15:32:54 +0300

    Added marker shadow

--- src/utils/icon.ts
+++ src/utils/icon.ts
@@ -1,13 +1,22 @@
 import L from 'leaflet';
 
-export function getIcon(id: any) : L.Icon {
+export function getIcon(id: any) : L.DivIcon {
     const url = getIconURL(id)
 
     return url ?
-    new L.Icon({
-        iconUrl: url,
-        iconSize: new L.Point(36, 36)
-    }) : new L.Icon({
+    new L.DivIcon({
+        html: `
+            <div style="
+            background-image: url('${url}');
+            background-size: cover;
+            width: 36px;
+            height: 36px;
+            filter: drop-shadow(0 0 2px white);
+            "></div>
+        `,
+        className: '', // Убираем стандартные стили Leaflet
+        iconSize: [36, 36]
+    }) : new L.DivIcon({
         iconUrl: '/revelation-online-map/icon/missing.svg',
         iconSize: new L.Point(20, 20)
     })
